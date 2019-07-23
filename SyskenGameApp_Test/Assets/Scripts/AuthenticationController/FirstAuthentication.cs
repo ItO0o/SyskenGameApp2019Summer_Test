c9343c9e5078051405f94d9d6716b154a4a6f437
ItO0o <k17014kk@aitech.ac.jp>
2019-07-23 18:24:50 +0900

    【追加】初期ユーザー設定シーン

--- SyskenGameApp_Test/Assets/Scripts/AuthenticationController/FirstAuthentication.cs
+++ SyskenGameApp_Test/Assets/Scripts/AuthenticationController/FirstAuthentication.cs
@@ -1,16 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameSparks.Api;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
+using UnityEngine.UI;
+using System;
+using System.IO;
 
 public class FirstAuthentication : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    string displayName = "";
+    string password = "";
+    string userName = "";
+    bool? newPlayer = true;
+    string userId = "";
+
+    public static string authToken = "";
+
+    InputField inputField;
+    Text warningText;
+
+    string SAVE_FILE_PATH = "Save/SaveData.json";
+
+    void Awake() {
+        inputField = GameObject.Find("Name_TextField").GetComponent<InputField>();
+        warningText = GameObject.Find("Warning_Text").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    public void RegistrationRequest() {
+        if (inputField.text.Length > 0) {
+            warningText.text = "";
+            displayName = UnityEngine.Random.Range(0, 1000000).GetHashCode().ToString();
+            userName = UnityEngine.Random.Range(0, 1000000).GetHashCode().ToString();
+            password = "0000";
+        } else {
+            warningText.text = "Please Input Name";
+            return;
+        }
+
+        new RegistrationRequest()
+          .SetDisplayName(displayName)
+          .SetPassword(password)
+          .SetUserName(userName)
+          .Send((response) => {
+            authToken = response.AuthToken;
+            newPlayer = response.NewPlayer;
+            RegistrationResponse._Player switchSummary = response.SwitchSummary;
+            userId = response.UserId;
+      });
+
+        if (newPlayer.Value == false) {
+            RegistrationRequest();
+        } else {
+            StaticInfo.userInfo.dispName = this.inputField.text;
+            StaticInfo.userInfo.first = false;
+            StaticInfo.userInfo.passWord = this.password;
+            StaticInfo.userInfo.userName = this.userName;
+
+            var json = JsonUtility.ToJson(StaticInfo.userInfo);
+            var path = Application.dataPath + "/" + SAVE_FILE_PATH;
+            var writer = new StreamWriter(path, false);
+            Debug.Log(json);
+            writer.WriteLine(json);
+            writer.Flush();
+            writer.Close();
+
+            Debug.Log("New Player:" + userId.ToString());
+        }
+    }
 }
