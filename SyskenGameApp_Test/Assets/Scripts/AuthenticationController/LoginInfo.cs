4cdb775d0857c8117eb2a9aadc7fc29c19962ee6
ItO0o <k17014kk@aitech.ac.jp>
2019-07-23 18:25:25 +0900

    【メタ更新】

---
+++ SyskenGameApp_Test/Assets/Scripts/AuthenticationController/LoginInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct LoginInfo {
+    public bool first;
+    public string userName;
+    public string passWord;
+    public string dispName;
+}
---
+++ SyskenGameApp_Test/Assets/Scripts/AuthenticationController/TransitionSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class TransitionSelector : MonoBehaviour {
+    string SAVE_FILE_PATH = "Save/SaveData.json";
+    // Use this for initialization
+    void Start () {
+        var info = new FileInfo(Application.dataPath + "/" + SAVE_FILE_PATH);
+        var reader = new StreamReader(info.OpenRead());
+        var json = reader.ReadToEnd();
+        LoginInfo data = JsonUtility.FromJson<LoginInfo>(json);
+        if (StaticInfo.userInfo.first == true) {
+            SceneManager.LoadScene("Login");
+        } else {
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
