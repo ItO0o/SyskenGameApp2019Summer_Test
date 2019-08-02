3056f38a67f1d3cafc3f075b6bea3065df570dce
ItO0o <k17014kk@aitech.ac.jp>
2019-08-02 18:26:00 +0900

    【追加】いっぱい

---
+++ SyskenGameApp_Test/Assets/Scripts/Photon/ConnectionPhoton.cs
@@ -0,0 +1,75 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+// MonoBehaviourではなくMonoBehaviourPunCallbacksを継承して、Photonのコールバックを受け取れるようにする
+public class ConnectionPhoton : MonoBehaviourPunCallbacks {
+
+    string roomName = "";
+    int roomCnt = 0;
+    string searchRoomName = "room";
+    public static SearchState state = SearchState.CreateRoom;
+
+    public enum SearchState {
+        CreateRoom,
+        JoinRoom
+    }
+
+    private void Start() {
+        // PhotonServerSettingsに設定した内容を使ってマスターサーバーへ接続する
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void StartMatching() {
+        CreateRoom();
+    }
+    // マスターサーバーへの接続が成功した時に呼ばれるコールバック
+    public override void OnConnectedToMaster() {
+    }
+
+    public void CreateRoom(){
+        // "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
+        RoomOptions room = new RoomOptions();
+        room.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom(searchRoomName + roomCnt, room, TypedLobby.Default);
+    }
+
+    public void SearchRoom() {
+        PhotonNetwork.JoinRoom(searchRoomName + roomCnt);
+    }
+
+    // マッチングが成功した時に呼ばれるコールバック
+    public override void OnJoinedRoom() {
+        PhotonNetwork.LoadLevel("Battle");
+        // マッチング後、ランダムな位置に自分自身のネットワークオブジェクトを生成する
+        //var v = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        //PhotonNetwork.Instantiate("Player", v, Quaternion.identity);
+    }
+
+    //JoinRoomが失敗したときに呼ばれる
+    void OnPhotonJoinRoomFailed(object[] codeAndMsg) {
+        //codeAndMsg[0]は エラーコード です。(int)
+        //codeAndMsg[1]は デバッグメッセージ です。(string)
+        if (roomCnt > 5 && state == SearchState.JoinRoom) {
+            roomCnt = 0;
+            Debug.Log("Failed Matching Try Again");
+            return;
+        }
+        if (roomCnt > 5){
+            Debug.Log("Search Room");
+            roomCnt = 0;
+            state = SearchState.JoinRoom;
+        }
+        roomCnt++;
+        if (state == SearchState.CreateRoom) {
+            CreateRoom();
+        }else if (state == SearchState.JoinRoom) {
+            SearchRoom();
+        }
+    }
+
+    void OnPhotonCreateRoomFailed(object[] codeAndMsg) {
+        //codeAndMsg[0]は エラーコード です。(int)
+        //codeAndMsg[1]は デバッグメッセージ です。(string)
+    }
+}
---
+++ SyskenGameApp_Test/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    [SerializeField]
+    float speed, acceleration = 0, rate, forwardMaxSpeed, backMaxSpeed;
+    
+
+
+#if UNITY_EDITOR
+    // Update is called once per frame
+    void FixedUpdate() {
+        if ((Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) || (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))) {
+            acceleration = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) {
+            acceleration = 0;
+        }
+        if (ConnectionPhoton.state == ConnectionPhoton.SearchState.JoinRoom) {
+            if (Input.GetKey(KeyCode.A)) {
+                MoveForward();
+            }
+            if (Input.GetKey(KeyCode.D)) {
+                MoveBack();
+            }
+        }else if(ConnectionPhoton.state == ConnectionPhoton.SearchState.CreateRoom) {
+            if (Input.GetKey(KeyCode.D)) {
+                MoveBack();
+            }
+            if (Input.GetKey(KeyCode.A)) {
+                MoveForward();
+            }
+        }
+    }
+#endif
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    void MoveForward() {
+        if (acceleration < forwardMaxSpeed) {
+            acceleration += rate;
+            }
+        this.transform.position += this.transform.right * speed * acceleration;
+    }
+
+    void MoveBack() {
+        if (acceleration < backMaxSpeed) {
+            acceleration += rate;
+            }
+        this.transform.position += -this.transform.right * speed * acceleration;
+    }
+
+    private void Move() {
+
+    }
+}
