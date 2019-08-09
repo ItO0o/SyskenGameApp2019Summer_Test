164c53e62255971eb77837af3aa14b84bbf4b986
ItO0o <k17014kk@aitech.ac.jp>
2019-08-09 21:55:43 +0900

    【テスト】GameSparksへのデータ送信、からの引き出し

--- SyskenGameApp_Test/Assets/Scripts/AuthenticationController/Test.cs
+++ SyskenGameApp_Test/Assets/Scripts/AuthenticationController/Test.cs
@@ -9,19 +9,79 @@ public class Test : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        //new AuthenticationRequest()
-            //.SetPassword("0000")
-            //.SetUserName("635472")
-                    //.Send((response) => {
-                    //    if (!response.HasErrors) {
-                    //        Debug.Log("Login successfully");
-                    //    } else {
-                    //        Debug.Log("Error Login");
-                    //    }
-                    //});
+        inventory();
+        //playerdata();
+    }
+
+    void inventory() {
+        new AuthenticationRequest()
+.SetPassword("0000")
+.SetUserName("334870")
+     .Send((response) => {
+         if (!response.HasErrors) {
+             Debug.Log("Login successfully");
+         } else {
+             Debug.Log("Error login");
+         }
+     });
+
+        Item item = new Item { name = "test1", cnt = 1 };
+        Item item2 = new Item { name = "test2", cnt = 3 };
+        //Debug.Log(StaticInfo.userData.items.name);
+        StaticInfo.items.Add(item);
+        StaticInfo.items.Add(item2);
+        string jsonData = JsonUtility.ToJson(new Serialization<Item>(StaticInfo.items));
+        //string jsonData = JsonUtility.ToJson(items);
+        GameSparks.Core.GSRequestData data = new GameSparks.Core.GSRequestData(jsonData);
+        Debug.Log(jsonData);
+        new LogEventRequest()
+        .SetEventKey("SaveInventory")
+        .SetEventAttribute("inventory", data)
+        .Send((response) => {
+            if (!response.HasErrors) {
+                Debug.Log("Save inventory successfully");
+            } else {
+                Debug.Log("Error save inventory");
+            }
+        });
+
+        new LogEventRequest()
+    .SetEventKey("LoadInventory")
+    .Send((response) => {
+        if (!response.HasErrors) {
+            GameSparks.Core.GSData scriptData = response.ScriptData.GetGSData("data");
+            var temp = scriptData.GetGSDataList("target");
+
+            List<Item> items = new List<Item>();
+            for (int i = 0;i < temp.Count;i++) {
+                Item tempItem = new Item {
+                    name = temp[i].GetString("name"),
+                    cnt = (int)temp[i].GetInt("cnt")
+                };
+                StaticInfo.items.Add(tempItem);
+            }
+
+            foreach(Item tempItem in StaticInfo.items) {
+                Debug.Log("name:" + tempItem.name + "cnt:" + tempItem.cnt);
+            }
+        } else {
+            Debug.Log("Error Load Player Data");
+        }
+    });
+    }
 
 
-
+    void playerdata() {
+        new AuthenticationRequest()
+    .SetPassword("0000")
+    .SetUserName("26337")
+            .Send((response) => {
+                if (!response.HasErrors) {
+                    Debug.Log("Login successfully");
+                } else {
+                    Debug.Log("Error login");
+                }
+            });
 
         //Item item = new Item { name = "test1", cnt = 1 };
         //Item item2 = new Item { name = "test2", cnt = 3 };
@@ -30,26 +90,36 @@ public class Test : MonoBehaviour {
         //StaticInfo.userData.items.Add(item2);
         Debug.Log("Saving");
         PlayerData player = new PlayerData {
-            gold = 0,
+            gold = 2,
             rate = 0
         };
         string jsonData = JsonUtility.ToJson(player);
         GameSparks.Core.GSRequestData data = new GameSparks.Core.GSRequestData(jsonData);
+        Debug.Log(jsonData);
+        new LogEventRequest()
+                .SetEventKey("SavePlayerData")
+                .SetEventAttribute("playerData", data)
+                .Send((response) => {
+                    if (!response.HasErrors) {
+                        Debug.Log("Save data successfully");
+                    } else {
+                        Debug.Log("Error Save Player Data");
+                    }
+                });
 
         new LogEventRequest()
-            .SetEventKey("SavePlayerData")
-            .SetEventAttribute("playerData", data)
+            .SetEventKey("LoadPlayerData")
             .Send((response) => {
                 if (!response.HasErrors) {
-                    Debug.Log("Save data successfully");
+                    GameSparks.Core.GSData scriptData = response.ScriptData.GetGSData("data");
+                    PlayerData playerData = new PlayerData {
+                        rate = (int)scriptData.GetInt("rate"),
+                        gold = (int)scriptData.GetInt("gold")
+                    };
+                    Debug.Log("rate:" + playerData.rate + "gold:" + playerData.gold);
                 } else {
-                    Debug.Log("Error Saving Data");
+                    Debug.Log("Error Load Player Data");
                 }
             });
-    }
-
-    // Update is called once per frame
-    void Update() {
-
     }
 }
