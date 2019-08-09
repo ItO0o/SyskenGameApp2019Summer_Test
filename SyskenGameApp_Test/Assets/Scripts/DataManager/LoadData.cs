4f0ff2c61a0ddd6a8c548a0a27e8c101b9ddbd21
ItO0o <k17014kk@aitech.ac.jp>
2019-08-09 22:23:16 +0900

    【追加】GameSparksデータ送受信

---
+++ SyskenGameApp_Test/Assets/Scripts/DataManager/LoadData.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameSparks.Api;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
+
+/// <summary>
+/// Load data or info to GameSparks.
+/// </summary>
+public class LoadData : MonoBehaviour {
+    //エラーチェック用
+    private bool check;
+
+    /// <summary>
+    /// ユーザーデータをGameSparkより引き出してStaticフィールドに反映します
+    /// </summary>
+    /// <returns><c>true</c>, ロード成功, <c>false</c> ロード失敗.</returns>
+    public bool LoadUserInfo() {
+        check = true;
+        //とりあえず保存先ユーザー指定,認証
+        new AuthenticationRequest()
+        .SetPassword("0000")
+        .SetUserName(StaticInfo.userInfo.userName)
+        .Send((response) => {
+            if (!response.HasErrors) {
+                Debug.Log("Login successfully");
+            } else {
+                Debug.Log("Error login");
+                check = false;
+            }
+        });
+        //イベントリクエスト
+        new LogEventRequest()
+         .SetEventKey("LoadPlayerData")
+         .Send((response) => {
+             if (!response.HasErrors) {
+                 GameSparks.Core.GSData scriptData = response.ScriptData.GetGSData("data");
+                 PlayerData playerData = new PlayerData {
+                     rate = (int)scriptData.GetInt("rate"),
+                     gold = (int)scriptData.GetInt("gold")
+                 };
+                 //スタティックフィールドに反映
+                 StaticInfo.userData.rate = playerData.rate;
+                 StaticInfo.userData.gold = playerData.gold;
+                 Debug.Log("Load user data (rate:" + playerData.rate + "gold:" + playerData.gold + ")");
+             } else {
+                 Debug.Log("Error Load Player Data");
+                 check = false;
+             }
+         });
+
+        if (check == false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Loads the inventory.
+    /// </summary>
+    /// <returns><c>true</c>, if inventory was loaded, <c>false</c> otherwise.</returns>
+    public bool LoadInventory() {
+        check = true;
+        //イベントリクエスト
+        new LogEventRequest()
+          .SetEventKey("LoadInventory")
+          .Send((response) => {
+              if (!response.HasErrors) {
+                  GameSparks.Core.GSData scriptData = response.ScriptData.GetGSData("data");
+                  var temp = scriptData.GetGSDataList("target");
+
+                  //受信データを整形、スタティックフィールドに反映
+                  for (int i = 0; i < temp.Count; i++) {
+                      Item tempItem = new Item {
+                          name = temp[i].GetString("name"),
+                          cnt = (int)temp[i].GetInt("cnt")
+                      };
+                      StaticInfo.items.Add(tempItem);
+                  }
+
+                  foreach (Item tempItem in StaticInfo.items) {
+                      Debug.Log("Load inventory　(name:" + tempItem.name + "cnt:" + tempItem.cnt+")");
+                  }
+              } else {
+                  Debug.Log("Error Load Player Data");
+                  check = false;
+              }
+          });
+
+        if (check == false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
---
+++ SyskenGameApp_Test/Assets/Scripts/DataManager/SendData.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameSparks.Api;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
+
+/// <summary>
+/// Send data or info to GameSparks.
+/// </summary>
+public class SendData : MonoBehaviour {
+    //エラーチェック用
+    private bool check;
+
+    /// <summary>
+    /// Sends the user info.
+    /// </summary>
+    /// <returns><c>true</c>, if user info was sent, <c>false</c> otherwise.</returns>
+    public bool SendUserInfo() {
+        check = true;
+        //とりあえず保存先ユーザー指定,認証
+        new AuthenticationRequest()
+       .SetPassword("0000")
+       .SetUserName(StaticInfo.userInfo.userName)
+               .Send((response) => {
+                   if (!response.HasErrors) {
+                       Debug.Log("Authentication successfully");
+                   } else {
+                       Debug.Log("Error authentication");
+                       check = false;
+                   }
+               });
+
+        //プレイヤーデータ整形
+        PlayerData player = new PlayerData {
+            gold = StaticInfo.userData.gold,
+            rate = StaticInfo.userData.rate
+        };
+        //jsonに変換
+        string jsonData = JsonUtility.ToJson(player);
+        GameSparks.Core.GSRequestData data = new GameSparks.Core.GSRequestData(jsonData);
+        //送信イベントリクエスト
+        new LogEventRequest()
+                .SetEventKey("SavePlayerData")
+                .SetEventAttribute("playerData", data)
+                .Send((response) => {
+                    if (!response.HasErrors) {
+                        Debug.Log("Save data successfully");
+                    } else {
+                        Debug.Log("Error save player data");
+                        check = false;
+                    }
+                });
+
+        if (check == false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Sends the inventory data.
+    /// </summary>
+    /// <returns><c>true</c>, if inventory data was sent, <c>false</c> otherwise.</returns>
+    public bool SendInventoryData() {
+
+        check = true;
+
+        new AuthenticationRequest()
+            .SetPassword("0000")
+            .SetUserName(StaticInfo.userInfo.userName)
+            .Send((response) => {
+            if (!response.HasErrors) {
+                Debug.Log("Login successfully");
+            } else {
+                Debug.Log("Error login");
+                    check = false;
+            }
+        });
+        //データ整形（シリアライズ込み）
+        string jsonData = JsonUtility.ToJson(new Serialization<Item>(StaticInfo.items));
+        GameSparks.Core.GSRequestData data = new GameSparks.Core.GSRequestData(jsonData);
+        //送信リクエスト
+        new LogEventRequest()
+        .SetEventKey("SaveInventory")
+        .SetEventAttribute("inventory", data)
+        .Send((response) => {
+            if (!response.HasErrors) {
+                Debug.Log("Save inventory successfully");
+            } else {
+                Debug.Log("Error save inventory");
+            }
+        });
+
+        if (check == false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
