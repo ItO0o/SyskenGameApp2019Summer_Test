5e0210ff2d515519d32fb6d515510635ca3d31a7
ItO0o <k17014kk@aitech.ac.jp>
2019-08-13 16:27:19 +0900

    【変更】unityの使用バージョン変更

--- SyskenGameApp_Test/Assets/Scripts/AuthenticationController/TesTes.cs
+++ SyskenGameApp_Test/Assets/Scripts/AuthenticationController/TesTes.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameSparks.Api;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
 
 public class TesTes : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        StaticInfo.userInfo.userName = "235211";
-        StaticInfo.userInfo.passWord = "0000";
-        StaticInfo.userData.rate = 100;
-        StaticInfo.userData.gold = 2500;
-        StaticInfo.items.Add(new Item { name = "Item1",cnt = 10});
-        StaticInfo.items.Add(new Item { name = "Item2", cnt = 0 });
-        StaticInfo.items.Add(new Item { name = "Item3", cnt = 5 });
-        SendData sd = new SendData();
-        sd.SendUserInfo();
-        sd.SendInventoryData();
+        //StaticInfo.userInfo.userName = "235211";
+        //StaticInfo.userInfo.passWord = "0000";
+        //StaticInfo.userData.rate = 100;
+        //StaticInfo.userData.gold = 2500;
+        //StaticInfo.items.Add(new Item { name = "Item1",cnt = 10});
+        //StaticInfo.items.Add(new Item { name = "Item2", cnt = 0 });
+        //StaticInfo.items.Add(new Item { name = "Item3", cnt = 5 });
+        //SendData sd = new SendData();
+        //sd.SendUserInfo();
+        //sd.SendInventoryData();
+        new LogEventRequest()
+         .SetEventKey("Get_Item_Drop")
+         .Send((response) => {
+             if (!response.HasErrors) {
+                 GameSparks.Core.GSData scriptData = response.ScriptData.GetGSData("data");
+                 Debug.Log(scriptData.GetInt("name"));
+             } else {
+                 Debug.Log("Error");
+             }
+         });
     }
 }
