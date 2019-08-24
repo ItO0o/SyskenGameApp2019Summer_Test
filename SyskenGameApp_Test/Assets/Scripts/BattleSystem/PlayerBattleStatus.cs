1f1ac3c0d126b3c2ba6d6f9ede071288b2cd91d6
KoyGara <ppp19971011@hotmail.co.jp>
2019-08-25 02:27:13 +0900

    【一時保存】

--- SyskenGameApp_Test/Assets/Scripts/BattleSystem/PlayerBattleStatus.cs
+++ SyskenGameApp_Test/Assets/Scripts/BattleSystem/PlayerBattleStatus.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerBattleStatus : MonoBehaviour
 {
-    public int hp = 100;
-    public int speed = 100;
+    public  int hp = 100;
+    public  int speed = 100;
+    public  int maxHP = 100;
 }
