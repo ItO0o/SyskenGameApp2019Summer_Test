736b2fdc284b48c079ab606da44dd1fe9319232c
ItO0o <k17014kk@aitech.ac.jp>
2019-08-26 11:05:43 +0900

    【ふえた】

---
+++ SyskenGameApp_Test/Assets/Scripts/BattleSystem/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public static float time = 0;
+    public int timeInt;
+    private Text timeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeText = GameObject.Find("Time").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StaticInfo.gameReady) {
+            time += Time.deltaTime;
+        }
+        PrintTime();
+    }
+
+    void PrintTime() {
+        timeText.text = ((int)time).ToString();
+    }
+}
---
+++ SyskenGameApp_Test/Assets/Scripts/CustomSystem/CustomCount.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomCount : MonoBehaviour
+{
+    Text countText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countText = GameObject.Find("CustomCount_Text").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countText.text = StaticInfo.myCustom.Count.ToString() + "/" + "20";
+    }
+}
